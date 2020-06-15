5285160fab75e95297f5b99cbcd8fd9e6ac38dff
suyun-sys <64997325+suyun-sys@users.noreply.github.com>
2020-06-15 17:18:13 +0900

    update 06/15

---
+++ Weekend/src/ForEx1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        
+		Scanner sc = new Scanner(System.in);
+	       
+    	int N= sc.nextInt();
+    	
+    	for(int i=1; i<=N; i++)
+    	{
+    		System.out.println(i);
+    	}
+	}
+
+}
